4a21aec1e41086789d55c3a1944cea9a10ff0347
Itsfoss0 <akhonyaebenezer@gmail.com>
2025-02-25 01:07:05 +0300

    [bug]: fix only error's getting logged

--- project/backend/src/index.js
+++ project/backend/src/index.js
@@ -9,9 +9,9 @@ const api = express();
 api.use(express.json());
 api.use(cors());
 
+api.use(logger('common'));
 api.use('/api/v1/', todoRouter);
 api.use('/_', probesRouter);
-api.use(logger('common'));
 
 api.use((req, res) => {
   return res.status(404).json({ error: 'not found' });
